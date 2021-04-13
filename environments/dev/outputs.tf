4f0351bef9ffa96b5cd39a23b44b0ca6d5b048e4
Shani Levy <shaniiiiii@gmail.com>
2021-04-13 17:00:50 +1000

    Update outputs.tf

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -34,11 +34,11 @@ output "external_ip" {
 }
 
 output "bigquery_dataset" {
-  value       = "${module.bigquery.bigquery_dataset}"
-  description = "Bigquery dataset resource."
+  value       = module.bigquery.bigquery_dataset
+  #description = "Bigquery dataset resource."
 }
 
 output "bigquery_tables" {
-  value       = "${module.bigquery.bigquery_tables}"
-  description = "Map of bigquery table resources being provisioned."
+  value       = module.bigquery.bigquery_tables
+  #description = "Map of bigquery table resources being provisioned."
 }
