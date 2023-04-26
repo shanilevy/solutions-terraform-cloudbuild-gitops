881be5dbfaa3dec4bca67aa319fc6d2a4d19677f
Shani Levy <shaniiiiii@gmail.com>
2023-04-26 15:22:02 +1000

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -12,11 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-locals {
-  env = "dev"
-}
-
 provider "google" {
   project = var.project_name
   region = var.region
