d0bf69c4dfc519fb408ea458808be3e5552c868a
Shani Levy <shaniiiiii@gmail.com>
2023-04-26 15:21:20 +1000

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -23,8 +23,12 @@ provider "google" {
   zone = var.zone
 }
 
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
 resource "google_storage_bucket" "default" {
-  name          = "dataops-terraform-tfstate"
+  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
   force_destroy = false
   location      = "US"
   storage_class = "STANDARD"
