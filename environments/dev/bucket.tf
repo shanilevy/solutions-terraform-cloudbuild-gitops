88dbf293757de1cac5d36fb16f7c05daf6a30ade
Shani Levy <shaniiiiii@gmail.com>
2023-04-26 12:29:16 +1000

    Delete bucket.tf

--- environments/dev/bucket.tf
+++
@@ -1,6 +0,0 @@
-# Create a GCS Bucket
-#resource "google_storage_bucket" "dataops_bucket" {
-#  name          = "my-dataops-bucket-123"
-#  location      = "us-central1"
-#  force_destroy = false
-#}
